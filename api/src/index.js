dd9e8460d154daa051b40755970cb4cf9a17fefd
Zach Smith <zachsmith.sa@gmail.com>
2019-07-07 12:11:08 +0200

    some experimentation with webapck. Still a WIP

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,22 @@
 import {
   createServer
 } from "http"
-const PORT = 9000
+const PORT = 3000
 
 createServer((request, response) => {
+  console.log('The origin is', request.headers.origin)
   const headers = {
     'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
     'Access-Control-Max-Age': 2592000, // 30 days
   }
   const allowedOrigins = [
+    'http://localhost:8080',,
     'http://localhost:8081',
     'http://localhost:8082'
   ]
   const origin = request.headers.origin
   if (allowedOrigins.indexOf(origin) > -1) headers['Access-Control-Allow-Origin'] = origin
-  console.log('The origin is', request.headers.origin)
+  
 
   if (request.method === 'OPTIONS') {
     response.writeHead(204, headers)
@@ -24,7 +26,7 @@ createServer((request, response) => {
 
   if (['GET', 'POST'].indexOf(request.method) > -1) {
     response.writeHead(200, headers)
-    response.end('Hello World')
+    response.end('Hello World 2')
     return
   }
 
