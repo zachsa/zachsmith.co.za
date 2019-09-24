cd51444548fcd4cda724e845ccce138bf81a3873
Zach Smith <zachsmith.sa@gmail.com>
2019-09-24 18:15:38 +0200

    improved link formatting

--- client-browser/src/pages/home.jsx
+++ client-browser/src/pages/home.jsx
@@ -12,10 +12,10 @@ export default () => (
         <Cell phoneSize={6} tabletSize={2} size={2}>
           <div style={{textAlign: "left"}}>
             <p>Hi, I'm Zach!</p>
-            <p><a target="_blank" href="https://github.com/zachsa">Github</a></p>
-            <p><a target="_blank" href="https://www.linkedin.com/in/zachsmithsouthafrica/">LinkedIn</a></p>
-            <p><a target="_blank" href="https://stackoverflow.com/users/3114742">StackOverflow</a></p>
-            <p><a target="_blank" href="https://open.spotify.com/album/0HCL1uiaVM9tSyJzhpZIq7">Album</a> ← Click this!!</p>
+            <p><a className="link" target="_blank" href="https://github.com/zachsa">Github</a></p>
+            <p><a className="link" target="_blank" href="https://www.linkedin.com/in/zachsmithsouthafrica/">LinkedIn</a></p>
+            <p><a className="link" target="_blank" href="https://stackoverflow.com/users/3114742">StackOverflow</a></p>
+            <p><a className="link" target="_blank" href="https://open.spotify.com/album/0HCL1uiaVM9tSyJzhpZIq7">Album</a> ← Click this!!</p>
           </div>
         </Cell>
         <Cell phoneSize={6} tabletSize={6} size={10}>
